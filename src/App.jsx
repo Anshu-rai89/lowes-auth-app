61d402c3f4552927dff7df395ee9fdf682d0b641
Anshu-rai89 <raianshu8991@gmail.com>
2025-06-14 09:46:48 +0530

    Fixed route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter as Router , Routes, Route}from 'react-router-dom'
+import {HashRouter as Router , Routes, Route}from 'react-router-dom'
 import HomePage from './Pages/HomePage'
 import LoginPage from './Pages/LoginPage'
 import ProfilePage from './Pages/ProfilePage'
